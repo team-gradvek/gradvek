9fe7816435e54d8b905a92a58fa46504c9642f59
kevinpatel24 <kep952@g.harvard.edu>
2023-04-01 17:07:28 -0500

    updated get_datasets.py file to support looping over all files at url

--- datasets/get_datasets.py
+++ datasets/get_datasets.py
@@ -1,33 +1,53 @@
+import wget
+import os
 import urllib.request
 from urllib.parse import urlparse
 from datetime import datetime
 import posixpath
 
-# path = urlparse.urlsplit(url).path
-# filename = posixpath.basename(path)
 
 def main():
-
     print("Start Program... ")
 
     try:
-        print("Start Downloading file... ")
+        print("Start Downloading files... ")
 
         # Get current time
         timenow = datetime.now()
         timenow_iso = timenow.strftime('%Y-%m-%d')
+
+        # Specify the URL
+
         
-        # Retrieve data
-        url = 'http://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/targets/part-00000-7c4d21db-d777-42ee-ae66-67426a0369f1-c000.snappy.parquet'
+        url = 'https://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.04/output/etl/parquet/diseases/'
 
-        # Add output's file name and location
-        output = f'opentarget/OT_target_{timenow_iso}.parquet'
-        urllib.request.urlretrieve(url, output)
+        # Use the current directory as the output directory
+        output_dir = os.getcwd()
 
-        print(f"File {output} -- saved!")
+        # Use wget to retrieve the HTML content of the page
+        html_content = wget.download(url, out=output_dir)
+
+        # Extract the links to the files on the page
+        links = []
+        with open(os.path.join(output_dir, html_content), 'r') as f:
+            for line in f:
+                if 'href' in line:
+                    start = line.find('href="') + 6
+                    end = line.find('"', start)
+                    link = line[start:end]
+                    if link.endswith('.parquet'):
+                        links.append(url + link)
+
+        # Download the files
+        for link in links:
+            filename = os.path.basename(link)
+            output_file = os.path.join(output_dir, filename)
+            wget.download(link, out=output_file)
+
+        print("Files downloaded successfully!")
 
     except Exception as e:
-        print("Downloading file error: " + str(e))
+        print("Downloading files error: " + str(e))
 
 if __name__ == "__main__":
     main()
