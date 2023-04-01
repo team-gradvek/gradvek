a7eb596164cdadf0b522aede55783ab0d6d145d0
Alyssa Bédard <alb2693@g.harvard.edu>
2023-04-01 17:44:33 -0400

    gitingore update

--- datasets/ftp.py
+++ datasets/ftp.py
@@ -1,6 +1,7 @@
 from ftplib import FTP
 
 
+
 #https://stackoverflow.com/questions/2605119/downloading-a-directory-tree-with-ftplib
 
 ftp = FTP('ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/targets')  # connect to host, default port
