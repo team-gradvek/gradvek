c005258759c9a0ed526503afcb30c6af9b1c8536
David Aviles <aviles.david1@gmail.com>
2023-04-04 01:25:24 -0400

    complete typeahead search with API routes and sample db

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -6,5 +6,7 @@ app_name = "search"
 
 urlpatterns =  [
     path("api/descriptors", views.DescriptorListView.as_view(), name = "descriptors"),
+    path("api/targets", views.TargetListView.as_view(), name = "targets"),
+    path("api/adverse-events", views.AdverseEventListView.as_view(), name = "adverse-events"),
 
 ]
