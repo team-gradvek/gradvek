ca72f1ab21180a1519bb538ea76bf656e3a0a2b5
Sheely <nathansheely@gmail.com>
2023-04-07 17:35:09 -0500

    Updated parsing datasets to prepare for better node and edge handling

--- datasets/parse_datasets.py
+++ datasets/parse_datasets.py
@@ -1,120 +1,146 @@
 import os
 import pyarrow.parquet as pq
-import neo4j
 
+# Set dataset name
 dataset = "opentarget 23.02"
 
 def main():
-    # use the opentarget directory for the input files
+    # Get the current working directory
     current_dir = os.getcwd()
+    # Set the input directory for the opentarget data
     input_dir = f"{current_dir}/opentarget"
 
-    # iterate over each data type directory
+    # Define data_type_query_generators, a dictionary that maps data types to tuples (node_query_generator, edge_query_generator)
+    data_type_query_generators = {
+        "targets": (create_cypher_query_targets, None),
+        "fda": (create_cypher_query_adverse_events, None),
+        "molecule": (create_cypher_query_drugs, None),
+        "mechanismOfAction": (None, create_cypher_query_mechanism_of_action),
+        "mousePhenotypes": (create_cypher_query_mouse_phenotypes, None),
+        "diseases": (create_cypher_query_diseases, None)
+    }
+
+    # Iterate over each data type in the input directory
     for data_type in os.listdir(input_dir):
+        # Set the data_type_path
         data_type_path = os.path.join(input_dir, data_type)
+        # Check if the path is a directory
         if not os.path.isdir(data_type_path):
             continue
 
-        # get the list of parquet files in the data type directory
-        files = [file for file in os.listdir(data_type_path) if file.endswith(".parquet")]
+        # Iterate over query types (node and edge)
+        for query_type in ('node', 'edge'):
+            # Set the generator depending on the query type
+            if query_type == 'node':
+                generator = data_type_query_generators[data_type][0]
+            else:
+                generator = data_type_query_generators[data_type][1]
 
-        # iterate over each parquet file in the data type directory
-        for n, file in enumerate(files):
-            if not file.endswith(".parquet"):
+            # Skip if the generator is None
+            if generator is None:
                 continue
-            file_path = os.path.join(data_type_path, file)
 
-            # read the parquet file as a pyarrow Table
+            # Generate and print queries for the current data type and query type
+            generate_queries(data_type, data_type_path, generator)
+
+def generate_queries(data_type, data_type_path, query_generator):
+    if query_generator is None:
+        return
+
+    # List all parquet files in the data_type_path
+    files = [file for file in os.listdir(data_type_path) if file.endswith(".parquet")]
+
+    # Iterate over each file and read its content
+    for n, file in enumerate(files):
+        print(f"Processing {data_type} file {n+1}/{len(files)}")
+        file_path = os.path.join(data_type_path, file)
+        try:
             table = pq.read_table(file_path)
+        except Exception as e:
+            print(f"Error reading file {file}: {e}")
+            continue
+        
+        try:
+            # Generate queries for the current table
+            queries = query_generator(table)
 
-            # print progress and first 5 rows of the file
-            print(f"{data_type} {n+1} of {len(files)}")
-            #print(table.to_pandas().head())
+            # Print queries for the current file
+            # TODO Replace this with neo4j function to evaluate the cypher queries
+            for query in queries:
+                print(query)
 
-            # generate cypher queries based on the data type
-            # each data type has a different parsing function
-            cypher_generator_functions_from_data = {
-                "targets": create_cypher_query_targets,
-                "fda": create_cypher_query_adverse_events,
-                "molecule": create_cypher_query_drugs,
-                "mechanismOfAction": create_cypher_query_mechanism_of_action,
-                "mousePhenotypes": create_cypher_query_mouse_phenotypes,
-                "diseases": create_cypher_query_diseases
-            }
+        except Exception as e:
+            print(f"Error generating queries for file {file}: {e}")
 
-            # execute the appropriate parsing function for the data type
-            if data_type in cypher_generator_functions_from_data:
-                func = cypher_generator_functions_from_data[data_type]
-                queries = func(table)
-                for query in queries:
-                    print(query)
-                    # execute the query in Neo4j
+# Generate Cypher queries for node creation based on a given table, node_label, and properties to columns mapping
+def create_cypher_query_nodes(table, node_label, props_to_columns):
+    # Convert the table to a pandas DataFrame
+    df = table.to_pandas()
+    queries = []
+    # Iterate over each row in the DataFrame
+    for _, row in df.iterrows():
+        # Generate properties string for the Cypher query
+        props = ', '.join([f"{prop}: '{row[column]}'" for prop, column in props_to_columns.items()])
+        props += f", dataset: '{dataset}'"  # Add the dataset constant value here
+        # Create a Cypher query for the current node
+        query = f"CREATE (:{node_label} {{{props}}})"
+        queries.append(query)
+    return queries
 
-    # a second pass is used to create the relationships between the nodes
-    # this include creating
-    # AssociatedWith relationships using the fda data
-    # Participates relationships using the targets data
-    # Action relationships that do not require additional data
-    # Pathway relationships that do not require additional data
-
-
-# parse the targets data and create a list of cypher queries to insert the data into the database
+# Generate Cypher queries for Target nodes
 def create_cypher_query_targets(table):
-    df = table.to_pandas()
-    queries = []
-    for _, row in df.iterrows():
-        query = f"CREATE (:Target {{name: '{row['approvedName']}', ensembleId: '{row['id']}', dataset: '{dataset}', symbol: '{row['approvedSymbol']}'}})"
-        queries.append(query)
-    return queries
+       return create_cypher_query_nodes(table, 'Target', {
+        'name': 'approvedName',
+        'ensembleId': 'id',
+        'symbol': 'approvedSymbol'
+    })
 
-# parse the adverse events data and create a list of cypher queries to insert the data into the database
+# Generate Cypher queries for AdverseEvent nodes
 def create_cypher_query_adverse_events(table):
-    df = table.to_pandas()
-    queries = []
-    for _, row in df.iterrows():
-        query = f"CREATE (:AdverseEvent {{meddraId: '{row['meddraCode']}', dataset: '{dataset}', adverseEventId: '{row['event']}'}})"
-        queries.append(query)
-    return queries
+    return create_cypher_query_nodes(table, 'AdverseEvent', {
+        'meddraId': 'meddraCode',
+        'adverseEventId': 'event'
+    })
 
-# parse the drugs data and create a list of cypher queries to insert the data into the database
+# Generate Cypher queries for Drug nodes
 def create_cypher_query_drugs(table):
-    df = table.to_pandas()
-    queries = []
-    for _, row in df.iterrows():
-        query = f"CREATE (:Drug {{drugId: '{row['name']}', dataset: '{dataset}', chemblId: '{row['id']}'}})"
-        queries.append(query)
-    return queries
+    return create_cypher_query_nodes(table, 'Drug', {
+        'drugId': 'name',
+        'chemblId': 'id'
+    })
 
-# parse the mechanism of action data and create a list of cypher queries to insert the data into the database
+# Generate Cypher queries for MousePhenotype nodes
+def create_cypher_query_mouse_phenotypes(table):
+    return create_cypher_query_nodes(table, 'MousePhenotype', {
+        'mousePhenotypeLabel': 'modelPhenotypeLabel',
+        'mousePhenotypeId': 'modelPhenotypeId'
+    })
+
+# Generate Cypher queries for Disease nodes
+def create_cypher_query_diseases(table):
+    return create_cypher_query_nodes(table, 'Disease', {
+        'name': 'name',
+        'diseaseId': 'id'
+    })
+
+# <TODO> Add function to handle generation of Cypher queries for edge creation similar to how nodes are handled
+# Parse the mechanism of action data and create a list of Cypher queries to insert the data into the database
 def create_cypher_query_mechanism_of_action(table):
+    # Convert the table to a pandas DataFrame
     df = table.to_pandas()
     queries = []
+    # Iterate over each row in the DataFrame
     for _, row in df.iterrows():
         if row['chemblIds'] is None or row['targets'] is None:
             continue
         for chemblId in row['chemblIds']:
             for target in row['targets']:
+                # Generate a Cypher query for the current mechanism of action relationship
                 query = f"MATCH (from:Drug), (to:Target)\nWHERE from.chemblId='{chemblId}'\nAND to.ensembleId='{target}'\nCREATE (from)-[:TARGETS {{dataset: '{dataset}'}}]->(to)"
                 queries.append(query)
     return queries
 
-# parse the mouse phenotypes data and create a list of cypher queries to insert the data into the database
-def create_cypher_query_mouse_phenotypes(table):
-    df = table.to_pandas()
-    queries = []
-    for _, row in df.iterrows():
-        query = f"CREATE (:MousePhenotype {{mousePhenotypeLabel: '{row['modelPhenotypeLabel']}', dataset: '{dataset}', mousePhenotypeId: '{row['modelPhenotypeId']}'}})"
-        queries.append(query)
-    return queries
-
-# parse the diseases data and create a list of cypher queries to insert the data into the database
-def create_cypher_query_diseases(table):
-    df = table.to_pandas()
-    queries = []
-    for _, row in df.iterrows():
-        query = f"CREATE (:Disease {{name: '{row['name']}', dataset: '{dataset}', diseaseId: '{row['id']}'}})"
-        queries.append(query)
-    return queries
-
+# Main function call
 if __name__ == "__main__":
     main()
+
