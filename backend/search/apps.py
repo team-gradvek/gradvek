9843eaa4a9514ea94f1de8244a2f0548a0d1f02f
Sheely <nathansheely@gmail.com>
2023-04-10 17:55:32 -0500

    starting paths

--- backend/search/apps.py
+++ backend/search/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from . import startup_functions
 
 
 class SearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "search"
+
+# https://stackoverflow.com/questions/6791911/execute-code-when-django-starts-once-only
+# Startup function appear to hook in here?
+    def ready(self):
+        
+        # Call your startup functions here
+        startup_functions.check_and_load_data()
