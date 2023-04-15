ebcbd38a576f15af1253b7755d851eb98bca9547
Sheely <nathansheely@gmail.com>
2023-04-15 17:52:57 -0500

    made count path generic

--- backend/search/utils.py
+++ backend/search/utils.py
@@ -1,7 +1,10 @@
 import re
 import string
 from django.urls import URLPattern, URLResolver
-from neomodel import db
+from neomodel import db, config
+from neomodel import NodeSet
+from neomodel.core import NodeMeta
+from neomodel.relationship import RelationshipMeta
 from .queries import ACTIONS, DATASETS
 
 
@@ -56,61 +59,19 @@ def get_all_routes(urlpatterns, prefix=''):
     return routes
 
 def get_entity_count(entity_type):
-    # Convert the input entity_type to lowercase for comparison
-    entity_type = entity_type.lower()
+    # Check if the entity_type is a relationship type or a node label, case-insensitively
+    rel_query = f"MATCH ()-[r]->() WHERE type(r) =~ '(?i){entity_type}' RETURN COUNT(r) LIMIT 1"
+    node_query = f"MATCH (n) WHERE any(label in labels(n) WHERE label =~ '(?i){entity_type}') RETURN COUNT(n) LIMIT 1"
 
-    # Define a mapping from lowercase entity types to their correct formats
-    entity_type_mapping = {
-        'adverseevent': 'AdverseEvent',
-        'drug': 'Drug',
-        'gene': 'Gene',
-        'target': 'Target',
-        'mousephenotype': 'MousePhenotype',
-        'pathway': 'Pathway',
-        'dataset': 'Dataset',
-        'associatedwith': 'AssociatedWith',
-        'mechanismofaction': 'MechanismOfAction',
-        'action': 'Action',
-        'involves': 'Involves',
-        'participates': 'Participates',
-    }
+    rel_count, _ = db.cypher_query(rel_query)
+    node_count, _ = db.cypher_query(node_query)
 
-    # Map the user input to the correct entity_type format
-    entity_type = entity_type_mapping.get(entity_type)
-    if entity_type is None:
-        raise ValueError(f"Invalid entity type: {entity_type}")
-
-    # Define a set of simple entity types
-    simple_entities = {
-        'AdverseEvent', 'Drug', 'Gene', 'Target', 'MousePhenotype', 'Pathway', 'Dataset'
-    }
-
-    # Define a dictionary of relation entity types with the related node labels
-    relation_entities = {
-        'AssociatedWith': ("Drug", "AdverseEvent"),
-        'MechanismOfAction': ("Drug", "Target"),
-        'Action': ("Drug", "Target"),
-        'Involves': ("Target", "Gene"),
-        'Participates': ("Target", "Pathway"),
-    }
-
-    # Check if the entity_type is a simple entity or a relation entity, and build the corresponding Cypher query
-    if entity_type in simple_entities:
-        query = f"MATCH (n:{entity_type}) RETURN COUNT(n)"
-    elif entity_type in relation_entities:
-        node1, node2 = relation_entities[entity_type]
-        query = f"MATCH (:{node1})-[n]->(:{node2}) RETURN COUNT(n)"
+    if rel_count[0][0] > 0:  # The entity_type is a relationship type
+        count_query = f"MATCH ()-[r]->() WHERE type(r) =~ '(?i){entity_type}' RETURN COUNT(r)"
+    elif node_count[0][0] > 0:  # The entity_type is a node label
+        count_query = f"MATCH (n) WHERE any(label in labels(n) WHERE label =~ '(?i){entity_type}') RETURN COUNT(n)"
     else:
         raise ValueError(f"Invalid entity type: {entity_type}")
 
-    # Execute the Cypher query and return the count of the requested entity type
-    results, _ = db.cypher_query(query)
-    return results[0][0]
-
-
-
-
-
-
-
-
+    count, _ = db.cypher_query(count_query)
+    return count[0][0]
