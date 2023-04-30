352570352926f02ff0b3d9facb2849289d2e2209
David Aviles <aviles.david1@gmail.com>
2023-04-30 01:36:22 -0400

    knowledge graphs and data tables for paths

--- frontend/src/hooks/drugsByTargetAEHook.js
+++ frontend/src/hooks/drugsByTargetAEHook.js
@@ -5,7 +5,7 @@ function getDrugsByTargetAE(target, ae) {
 
   const fetcher = url => axios.get(url).then(res => res.data)
 
-  const { data, error, isLoading } = useSWR(`http://localhost:8000/api/weight/${target}/${ae}`, fetcher)
+  const { data, error, isLoading } = useSWR(`http://localhost:8000/api/weight/${encodeURIComponent(target)}/${encodeURIComponent(ae)}`, fetcher)
  
   return {
     data: data,
