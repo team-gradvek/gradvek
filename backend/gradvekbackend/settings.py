1c61651d1c4016c8f8993c21c86128db1beb1427
Sheely <nathansheely@gmail.com>
2023-04-30 13:15:45 -0500

    set django env secret default none

--- backend/gradvekbackend/settings.py
+++ backend/gradvekbackend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY_DJANGO_ENV = env("SECRET_KEY")
+SECRET_KEY_DJANGO_ENV = env("SECRET_KEY", default=None)
 SECRET_KEY_OS_ENV = os.getenv("SECRET_KEY", default=None)
 
 if SECRET_KEY_OS_ENV:
