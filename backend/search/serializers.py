0b8c21c49c134103a08f3036e1aeceb9ca53f908
Sheely <nathansheely@gmail.com>
2023-05-05 19:20:32 -0500

    updated similarity serializer

--- backend/search/serializers.py
+++ backend/search/serializers.py
@@ -27,48 +27,45 @@ class NodeSimilaritySerializer(serializers.ModelSerializer):
 class MousePhenoSerializer(NodeSimilaritySerializer):
     class Meta:
         model = MousePheno
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class HgeneSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Hgene
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class HproteinSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Hprotein
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class IntactSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Intact
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class PathwaySerializer(NodeSimilaritySerializer):
     class Meta:
         model = Pathway
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class ReactomeSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Reactome
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class SignorSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Signor
-        fields = ["target2", "similarity"]
+        fields = ["target1", "target2", "similarity"]
 
 class GwasSerializer(NodeSimilaritySerializer):
     class Meta:
         model = Gwas
-        fields = ["target2", "similarity"]
-
-class AverageSimilaritySerializer(serializers.Serializer):
-    target2 = serializers.CharField()
-    average_similarity = serializers.FloatField()
+        fields = ["target1", "target2", "similarity"]
 
 class AverageNodeSimilaritySerializer(serializers.Serializer):
+    target1 = serializers.CharField()
     target2 = serializers.CharField()
     average = serializers.FloatField()
     descriptors = serializers.ListField(child=serializers.CharField())
