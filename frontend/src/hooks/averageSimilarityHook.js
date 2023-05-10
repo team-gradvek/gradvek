bafbc9090ea9dd7cc0fab50512038f4bb66ed071
Sheely <nathansheely@gmail.com>
2023-05-10 09:44:10 -0500

    fixed live path for average

--- frontend/src/hooks/averageSimilarityHook.js
+++ frontend/src/hooks/averageSimilarityHook.js
@@ -7,7 +7,7 @@ function averageSimilarityHook(id) {
 
   const fetcher = url => axios.get(url).then(res => res.data)
 
-  const { data, error, isLoading } = useSWR(`http://localhost:8000/api/average_similarity/${encodeURIComponent(id)}/`, fetcher)
+  const { data, error, isLoading } = useSWR(`http://www.gradvek.org:8083/api/average_similarity/${encodeURIComponent(id)}/`, fetcher)
 
   console.log(data)
   
